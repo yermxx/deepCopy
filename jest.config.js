7dec21b4ddaf514cec793292e1b165ed3eb686ad
yermxx <66yrlee99@gmail.com>
2024-11-16 16:42:22 +0900

    refactor: Update project

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { useESM: true }],
+    '^.+.tsx?$': ['ts-jest', {}],
   },
-  roots: ['<rootDir>/src'],
 };
--- src/js/index.test.js
+++
@@ -1,7 +0,0 @@
-import { add } from './index';
-describe('test - add', () => {
-  test('add', () => {
-    const sum = add(2, 3);
-    expect(sum).toBe(5);
-  });
-});
